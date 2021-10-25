5d388eb6eff58d8cd7bd4a2f22907367a43c8866
Palash Singhai <spalashsinghai22@gmail.com>
2021-10-25 15:10:50 +0530

    Add files via upload

---
+++ number_plate_detection_with_image.py
@@ -0,0 +1,83 @@
+import cv2
+import imutils
+import matplotlib.pyplot as plt
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+# Read the image file
+image = cv2.imread(r"D:\pythonProject\test_car_images\carimg.jpeg")
+
+# Resize the image - change width to 500
+# image = imutils.resize(image, width=500)
+
+# Display the original image
+cv2.imshow("Original Image", image)
+cv2.waitKey(0)
+
+# RGB to Gray scale conversion
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("1 - Grayscale Conversion", gray)
+cv2.waitKey(0)
+
+# Noise removal with iterative bilateral filter(removes noise while preserving edges)
+gray = cv2.bilateralFilter(gray, 11, 30, 30)
+cv2.imshow("2 - Bilateral Filter", gray)
+cv2.waitKey(0)
+
+# Find Edges of the grayscale image
+edged = cv2.Canny(gray, 100, 255)
+# cv2.imshow("3 - Canny Edges", edged)
+# cv2.waitKey(0)
+
+# Find contours based on Edges
+cnts, new  = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+# Create copy of original image to draw all contours
+img1 = image.copy()
+cv2.drawContours(img1, cnts, -1, (0,255,0), 3)
+# cv2.imshow("4- All Contours", img1)
+# cv2.waitKey(0)
+
+#sort contours based on their area keeping minimum required area as '30' (anything smaller than this will not be considered)
+cnts=sorted(cnts, key = cv2.contourArea, reverse = True)[:40]
+NumberPlateCnt = None #we currently have no Number plate contour
+
+# Top 30 Contours
+img2 = image.copy()
+cv2.drawContours(img2, cnts, -1, (0,255,0), 3)
+cv2.imshow("5- Top 30 Contours", img2)
+cv2.waitKey(0)
+
+# loop over our contours to find the best possible approximate contour of number plate
+count = 0
+NumberPlateCnt = []
+for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # print ("approx = ",approx)
+        if len(approx) == 4:  # Select the contour with 4 corners
+            NumberPlateCnt = approx #This is our approx Number Plate Contour
+
+            # Crop those contours and store it in Cropped Images folder
+            x, y, w, h = cv2.boundingRect(c) #This will find out co-ord for plate
+            new_img = gray[y:y + h, x:x + w] #Create new image
+
+
+            break
+
+
+# Drawing the selected contour on the original image
+#print(NumberPlateCnt)
+cv2.drawContours(image, NumberPlateCnt, -1, (0,255,0), 3)
+cv2.imshow("Final Image With Number Plate Detected", image)
+cv2.waitKey(0)
+
+Cropped = image[y:y+h,x:x+w]
+cv2.imshow("Cropped Image ", Cropped)
+cv2.waitKey(0)
+
+# Use tesseract to covert image into string
+text = pytesseract.image_to_string(Cropped)
+print("Number is :", text)
+
